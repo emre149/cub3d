82b18fb0da77458ad4e8e2f1e05687c9d1c337b5
Sacha Morin <smorin@bess-f2r4s4.clusters.42paris.fr>
2025-03-06 13:25:54 +0100

    Fix: correction du problème de chargement des textures

---
+++ src/init/init.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/02 14:25:18 by ededemog          #+#    #+#             */
+/*   Updated: 2025/03/03 16:29:03 by ededemog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+void	set_img(t_img *img)
+{
+	img->img_mlx = NULL;
+	img->addr = NULL;
+	img->line_length = 0;
+	img->bits_per_pixel = 0;
+	img->endian = 0;
+	img->width = 0;
+	img->height = 0;
+}
+
+void	init_mlx(t_map_info *map_info)
+{
+	map_info->mlx = mlx_init();
+	if (!map_info->mlx)
+		error_exit(map_info, "Failed to initialize mlx");
+	map_info->win = mlx_new_window(map_info->mlx, SCREEN_WIDTH, SCREEN_HEIGHT,
+			"Cub3D");
+	if (!map_info->win)
+	{
+		free(map_info->mlx);
+		error_exit(map_info, "Failed to create window");
+	}
+}
+
+void	xpm_to_img(t_map_info *map_info, t_img *img, char *tex)
+{
+	set_img(img);
+	img->img_mlx = mlx_xpm_file_to_image(map_info->mlx, tex, &img->width,
+			&img->height);
+	if (!img->img_mlx)
+	{
+		write(2, "Failed to load texture: ", 24);
+		write(2, tex, ft_strlen(tex));
+		write(2, "\n", 1);
+		error_exit(map_info, "Texture loading failed");
+	}
+	img->addr = (int *)mlx_get_data_addr(img->img_mlx, &img->bits_per_pixel,
+			&img->line_length, &img->endian);
+}
+
+static void	init_plane(t_map_info *map_info, char c)
+{
+	if (c == 'S')
+	{
+		map_info->plane[0] = -0.66;
+		map_info->plane[1] = 0;
+	}
+	if (c == 'N')
+	{
+		map_info->plane[0] = 0.66;
+		map_info->plane[1] = 0;
+	}
+	if (c == 'E')
+	{
+		map_info->plane[0] = 0;
+		map_info->plane[1] = 0.66;
+	}
+	if (c == 'W')
+	{
+		map_info->plane[0] = 0;
+		map_info->plane[1] = -0.66;
+	}
+}
+
+static void	init_spawn(int y, int x, char c, t_map_info *map_info)
+{
+	map_info->pos[0] = (double)x + 0.5;
+	map_info->pos[1] = (double)y + 0.5;
+	if (c == 'S')
+	{
+		map_info->dir[0] = 0;
+		map_info->dir[1] = 1;
+	}
+	else if (c == 'N')
+	{
+		map_info->dir[0] = 0;
+		map_info->dir[1] = -1;
+	}
+	else if (c == 'E')
+	{
+		map_info->dir[0] = 1;
+		map_info->dir[1] = 0;
+	}
+	else if (c == 'W')
+	{
+		map_info->dir[0] = -1;
+		map_info->dir[1] = 0;
+	}
+	init_plane(map_info, c);
+}
+
+void	map_pos(t_map_info *map_info)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (map_info->map[++i])
+	{
+		j = -1;
+		while (map_info->map[i][++j])
+		{
+			if (map_info->map[i][j] == 'N' || map_info->map[i][j] == 'S'
+				|| map_info->map[i][j] == 'E' || map_info->map[i][j] == 'W')
+				init_spawn(i, j, map_info->map[i][j], map_info);
+		}
+	}
+}
---
+++ src/init/init_all.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_all.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/02 14:25:44 by ededemog          #+#    #+#             */
+/*   Updated: 2025/03/03 17:23:00 by ededemog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+static int	*stock_tmp(t_map_info *map_info, char *path)
+{
+	t_img	tmp;
+	int		*array;
+	int		x;
+	int		y;
+
+	xpm_to_img(map_info, &tmp, path);
+	array = ft_calloc(1, sizeof(int) * (IMG_SIZE * IMG_SIZE));
+	if (!array)
+		error_exit(map_info, "Failed to allocate memory for texture");
+	y = -1;
+	while (++y < IMG_SIZE)
+	{
+		x = -1;
+		while (++x < IMG_SIZE)
+			array[y * IMG_SIZE + x] = tmp.addr[y * IMG_SIZE + x];
+	}
+	mlx_destroy_image(map_info->mlx, tmp.img_mlx);
+	return (array);
+}
+
+void	init_textures(t_map_info *map_info)
+{
+	map_info->assets = ft_calloc(5, sizeof(int *));
+	if (!map_info->assets)
+		error_exit(map_info, "Failed to allocate memory for textures array");
+	write(1, "Loading textures...\n", 20);
+	map_info->assets[NO] = stock_tmp(map_info, map_info->no_texture);
+	map_info->assets[SO] = stock_tmp(map_info, map_info->so_texture);
+	map_info->assets[WE] = stock_tmp(map_info, map_info->we_texture);
+	map_info->assets[EA] = stock_tmp(map_info, map_info->ea_texture);
+	write(1, "Textures loaded successfully.\n", 30);
+}
+
+void	launch_game(t_map_info *map_info)
+{
+	map_info->img = malloc(sizeof(t_img));
+	if (!map_info->img)
+		error_exit(map_info, "Failed to allocate memory for image");
+
+	init_mlx(map_info);
+	set_img(map_info->img);
+	map_pos(map_info);
+	init_textures(map_info);
+	map_info->move[X] = 0;
+	map_info->move[Y] = 0;
+	map_info->moved = 0;
+	map_info->rot_left = 0;
+	map_info->rot_right = 0;
+	raycasting(map_info);
+	mlx_hook(map_info->win, KeyPress, KeyPressMask, &key_hook, map_info);
+	mlx_hook(map_info->win, KeyRelease, KeyReleaseMask, &key_release, map_info);
+	mlx_hook(map_info->win, DestroyNotify, 0, &destroy_game,
+		map_info);
+	mlx_loop_hook(map_info->mlx, &moves, map_info);
+	mlx_loop(map_info->mlx);
+}
